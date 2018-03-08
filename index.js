c94f65094a66067bcc976d2dde71b66a5c194e1f
weeznog <drew.m.peterson@gmail.com>
2018-03-08 11:00:38 -0800

    update nodemon hopefully fix crash!

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./server/server');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on: http://localhost:${PORT}/graphiql`);
+  console.log(`GraphQL: http://localhost:${PORT}/graphiql`);
 });
