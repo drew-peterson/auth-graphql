305dc03fe3ceefcff48360cc187669b38c3573ec
weeznog <drew.m.peterson@gmail.com>
2018-03-08 08:50:14 -0800

    add env port to server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./server/server');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on: http://localhost:${PORT}/graphiql`);
