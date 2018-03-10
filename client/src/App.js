c89e24bde61c283caf6fbe6079ba737bd78a40fd
weeznog <drew.m.peterson@gmail.com>
2018-03-09 19:03:02 -0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import Landing from './components/Landing';
 import Dashboard from './components/Dashboard';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
-import requireLogin from './containers/requireLogin'; // HOC are lowercase because they are functions...
+// import requireLogin from './containers/requireLogin'; // HOC are lowercase because they are functions...
 
 // subscriptions =========================
 // Create an http link:
@@ -70,7 +70,8 @@ class App extends Component {
             <Route
               exact
               path="/dashboard"
-              component={requireLogin(Dashboard)}
+              // component={requireLogin(Dashboard)}
+              component={Dashboard}
             />
           </div>
         </Router>
