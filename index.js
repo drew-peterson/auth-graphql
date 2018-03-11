0f46b6e10a41fc33f9dc208195dfc3712f48095f
weeznog <drew.m.peterson@gmail.com>
2018-03-11 09:50:01 -0700

    test

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 // const server = require('./server/server');
-// const log = require('node-pretty-log');
-// const SubscriptionServer = require('subscriptions-transport-ws')
-//   .SubscriptionServer;
-// const subscribe = require('graphql').subscribe;
-// const execute = require('graphql').execute;
-//
-// const schema = require('./server/schema/schema');
+const log = require('node-pretty-log');
+const SubscriptionServer = require('subscriptions-transport-ws')
+  .SubscriptionServer;
+const subscribe = require('graphql').subscribe;
+const execute = require('graphql').execute;
 
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
 const passport = require('passport');
+const bodyParser = require('body-parser');
+const graphqlExpress = require('apollo-server-express').graphqlExpress;
+const graphiqlExpress = require('apollo-server-express').graphiqlExpress;
+// const models = require('./models');
+// const passportConfig = require('./services/auth');
+// const schema = require('./schema/schema');
+
 // const models = require('./models');
 // const keys = require('./config/keys');
 
@@ -35,15 +39,15 @@ const passport = require('passport');
 const app = express();
 // app.use(bodyParser.json());
 
-// const MONGO_URI =
-//   'mongodb://drew.peterson:peterson@ds157818.mlab.com:57818/auth-graphql';
-//
-// // Connect to the mongoDB instance and log a message
-// // on success or failure
-// mongoose.connect(MONGO_URI);
-// mongoose.connection
-//   .once('open', () => log('info', 'Connected to MongoLab instance.'))
-//   .on('error', error => log('error', 'Error connecting to MongoLab:', error));
+const MONGO_URI =
+  'mongodb://drew.peterson:peterson@ds157818.mlab.com:57818/auth-graphql';
+
+// Connect to the mongoDB instance and log a message
+// on success or failure
+mongoose.connect(MONGO_URI);
+mongoose.connection
+  .once('open', () => log('info', 'Connected to MongoLab instance.'))
+  .on('error', error => log('error', 'Error connecting to MongoLab:', error));
 
 // use cookies inside app
 app.use(
@@ -53,8 +57,26 @@ app.use(
   })
 );
 
+// PASSPORT
 app.use(passport.initialize());
 app.use(passport.session());
+
+// ROUTES - GraphQL
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress(req => ({ schema, context: req }))
+);
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: '/graphql'
+    // subscriptionsEndpoint: 'ws://localhost:5000/subscriptions'
+  })
+);
+
+// ROUTES - API
+// require('./routes/test')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -63,6 +85,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-const PORT = process.env.PORT || 5000;
+// const createServer = require('http').createServer;
+// module.exports = createServer(app);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
