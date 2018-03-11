e39e0d9180f8fb308582666d1569977686d00ff6
weeznog <drew.m.peterson@gmail.com>
2018-03-10 22:28:20 -0800

    test

--- index.js
+++ index.js
@@ -4,19 +4,10 @@ const SubscriptionServer = require('subscriptions-transport-ws')
   .SubscriptionServer;
 const subscribe = require('graphql').subscribe;
 const execute = require('graphql').execute;
-const express = require('express');
 
 const schema = require('./server/schema/schema');
 
 const PORT = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  server.use(express.static('client/build'));
-  server.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 server.listen(PORT, () => {
   new SubscriptionServer(
--- server/server.js
+++ server/server.js
@@ -58,4 +58,13 @@ app.use(
   })
 );
 
-module.exports = createServer(app);
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// module.exports = createServer(app);
+module.exports = app;
