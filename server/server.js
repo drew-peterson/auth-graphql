8eb2f5808f155d632230318c26852acf88ceba47
weeznog <drew.m.peterson@gmail.com>
2018-03-10 18:49:15 -0800

    test

--- server/server.js
+++ server/server.js
@@ -59,12 +59,14 @@ app.use(
 );
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('PRODUCTIONS SERVER.....');
   app.use(express.static('client/build'));
   const path = require('path');
-  app.get('*', (req, res) => {
-    console.log('APP *');
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
+  app.get('*', function(req, res) {
+    const index = path.join(__dirname, 'client', 'build', 'index.html');
+    res.sendFile(index);
   });
 }
 
