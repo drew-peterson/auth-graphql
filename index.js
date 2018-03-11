ae2afcd028ac06e85b2fddc44deff06f5b68931a
weeznog <drew.m.peterson@gmail.com>
2018-03-10 22:13:03 -0800

    fix spelling

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
+  server.use(express.static('client/build'));
+  server.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
